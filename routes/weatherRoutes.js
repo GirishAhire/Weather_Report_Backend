e93268a9a10fd751867b9b2032200dc4f9a1ecaf
GirishAhire <117669689+GirishAhire@users.noreply.github.com>
2025-08-29 10:27:57 +0530

    Error Solved

--- routes/weatherRoutes.js
+++ routes/weatherRoutes.js
@@ -3,27 +3,26 @@ const axios = require("axios");
 
 const router = express.Router();
 
-const WEATHER_API_BASE = process.env.WEATHER_API_BASE;
-const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+const API_URL = process.env.WEATHER_API_URL;
+const API_KEY = process.env.WEATHER_API_KEY;
 
+// GET weather by city
 router.get("/:city", async (req, res) => {
-    const city = req.params.city;
+    const { city } = req.params;
 
     try {
-        const response = await axios.get(
-            `${WEATHER_API_BASE}?q=${city}&appid=${WEATHER_API_KEY}&units=metric`
-        );
-
-        const data = response.data;
-
-        res.json({
-            city: data.name,
-            temperature: data.main.temp,
-            description: data.weather[0].description,
-            humidity: data.main.humidity,
-            wind_speed: data.wind.speed,
+        const response = await axios.get(API_URL, {
+            params: {
+                q: city,
+                appid: API_KEY,
+                units: "metric",
+            },
         });
+
+        res.json(response.data);
     } catch (error) {
+        console.error(error.response ? error.response.data : error.message);
+
         if (error.response) {
             res.status(error.response.status).json(error.response.data);
         } else {
